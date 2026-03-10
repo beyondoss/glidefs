520c8e77fa09c987b0634c67ed804df796b5d51b
Jared Lunde <jared.lunde@gmail.com>
2026-03-09 18:19:25 -0700

    add fuzz tests to ci

--- glidefs/src/task.rs
+++ glidefs/src/task.rs
@@ -2,36 +2,57 @@ use std::future::Future;
 use tokio::runtime::Handle;
 use tokio::task::JoinHandle;
 
-pub fn spawn_named<T, F>(name: &str, future: F) -> JoinHandle<T>
+pub fn spawn_named<T, F>(_name: &str, future: F) -> JoinHandle<T>
 where
     F: Future<Output = T> + Send + 'static,
     T: Send + 'static,
 {
-    tokio::task::Builder::new()
-        .name(name)
-        .spawn(future)
-        .expect("failed to spawn task")
+    #[cfg(tokio_unstable)]
+    {
+        tokio::task::Builder::new()
+            .name(_name)
+            .spawn(future)
+            .expect("failed to spawn task")
+    }
+    #[cfg(not(tokio_unstable))]
+    {
+        tokio::spawn(future)
+    }
 }
 
 #[allow(dead_code)]
-pub fn spawn_named_on<T, F>(name: &str, future: F, handle: &Handle) -> JoinHandle<T>
+pub fn spawn_named_on<T, F>(_name: &str, future: F, handle: &Handle) -> JoinHandle<T>
 where
     F: Future<Output = T> + Send + 'static,
     T: Send + 'static,
 {
-    tokio::task::Builder::new()
-        .name(name)
-        .spawn_on(future, handle)
-        .expect("failed to spawn task")
+    #[cfg(tokio_unstable)]
+    {
+        tokio::task::Builder::new()
+            .name(_name)
+            .spawn_on(future, handle)
+            .expect("failed to spawn task")
+    }
+    #[cfg(not(tokio_unstable))]
+    {
+        handle.spawn(future)
+    }
 }
 
-pub fn spawn_blocking_named<T, F>(name: &str, f: F) -> JoinHandle<T>
+pub fn spawn_blocking_named<T, F>(_name: &str, f: F) -> JoinHandle<T>
 where
     F: FnOnce() -> T + Send + 'static,
     T: Send + 'static,
 {
-    tokio::task::Builder::new()
-        .name(name)
-        .spawn_blocking(f)
-        .expect("failed to spawn blocking task")
+    #[cfg(tokio_unstable)]
+    {
+        tokio::task::Builder::new()
+            .name(_name)
+            .spawn_blocking(f)
+            .expect("failed to spawn blocking task")
+    }
+    #[cfg(not(tokio_unstable))]
+    {
+        tokio::task::spawn_blocking(f)
+    }
 }
